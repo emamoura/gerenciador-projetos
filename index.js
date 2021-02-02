dcae421387974b4b08dd6eec7b7f354f7c09a3b6
emamoura <emanuel.moura@ccc.ufcg.edu.br>
2021-02-02 12:37:49 -0300

    New routes and middleware

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const express = require('express');
 const app = express();
 
 const projects = [];
+
+
+function checkIfProjectExist(request, response, next) {
+    const { id } = request.params;
+    const project = projects.find((project => project.id === id));
+
+    if (!project) {
+        return response.status(400).json({ error: 'Project not found' })
+    }
+
+    return next();
+}
+
+function logRequests(request, response, next) {
+    console.count("Request number");
+
+    return next();
+}
+
+app.use(logRequests);
 app.use(express.json());
 
 app.get('/projects', (request, response) => {
@@ -9,21 +29,43 @@ app.get('/projects', (request, response) => {
 });
 
 app.post('/projects', (request, response) => {
-    const {id, title} = request.body;
-    const project = {id: id, title: title, tasks: []};
+    const { id, title } = request.body;
+    const project = { id: String(id), title: String(title), tasks: [] };
     projects.push(project);
     return response.json(project);
 });
 
-app.put('/projects/:id', (request, response) => {
-    const {id} = request.params;
-    const {title} = request.body;
+app.post('/projects/:id/tasks', checkIfProjectExist, (request, response) => {
+    const { id } = request.params;
+    const { title } = request.body;
+
+    const project = projects.find(project => project.id === id);
+
+    project.tasks.push(title);
+
+    return res.json(project);
+});
+
+app.put('/projects/:id', checkIfProjectExist, (request, response) => {
+    const { id } = request.params;
+    const { title } = request.body;
 
     const project = projects.find(project => project.id == id);
 
     project.title = title;
- 
+
     return response.json(project);
+});
+
+app.delete('/projects/:id', checkIfProjectExist, (request, response) => {
+    const { id } = request.params;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    projects.splice(projectIndex, 1);
+
+    return response.send('Sucess');
+
 });
 
 app.listen(3000, () => {
